9ae9dc557aef074437301f799f640fdcba6d665c
Hanadin <junhanlai@gmail.com>
2023-07-26 03:08:47 +0800

    Almost done
    TODO
    - Choose pictures
    - Settle fonts
    - Determine correct dimension
    - Video play after correct guess

--- script.js
+++ script.js
@@ -1,68 +1,59 @@
 window.onload = function() {
     var stages = [
       ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg', 'image6.jpg', 'image7.jpg', 'image8.jpg', 'image9.jpg'],
-      // ['image4.jpg', 'image5.jpg', 'image6.jpg'],
-      // ['image7.jpg', 'image8.jpg', 'image9.jpg']
     ];
   
-    var currentStage = 0;
+    var currentStage = 1;
     var images = document.getElementsByClassName('image');
-    // var revealButton = document.getElementById('reveal-button');
     var result = document.getElementById('result');
+    var bgId = "bgimage";
   
     // Attach click event handlers to all images
     for (var i = 0; i < images.length; i++) {
       images[i].addEventListener('click', function() {
-        // if (currentStage < stages.length && this.src.endsWith(stages[currentStage][i])) {
-        //   this.classList.add('selected');
-  
-        //   if (currentStage === stages.length - 1) {
-        //     revealButton.disabled = false;
-        //   }
-        // }
-        var filePath = this.src;
-        console.log(filePath);
-        if (filePath.includes("5.jpg")) {
-          result.style.display = "block";
-        }
         
-        // if (this.src.toString().) {
-        //   result.style.visibility = "visible";
-        // }
+        var filePath = this.src;
+        if (currentStage === 1 && filePath.includes("3.jpg")) {
+          result.style.display = "block";
+          showAnswerByColumn(2);
+          showAnswerByColumn(3);
+          triggerChangeImage(currentStage);
+        }
+
+        if (currentStage === 2 && filePath.includes("6.jpg")) {
+          showAnswerByColumn(4);
+          showAnswerByColumn(6);
+          triggerChangeImage(currentStage)
+        }
+
+        if (currentStage === 3 && filePath.includes("9.jpg")) {
+          showAnswerByColumn(1);
+          showAnswerByColumn(5);
+        }
       });
     }
-  
-    // Attach click event handler to reveal button
-    // revealButton.addEventListener('click', revealAnswer);
+
+    function triggerChangeImage(stage) {
+      currentStage = 0;
+      setTimeout(() => changeImage(stage), 1000);
+      currentStage = stage + 1; 
+    }
+
+    function changeImage(stage) {
+      var newStage = stage + 1;
+      var newBgImagePath = 'url(' + "stage" + newStage + "/image" + (newStage*3) + ".jpg)";
+      document.getElementById(bgId).style.backgroundImage = newBgImagePath;
+      for (let index = 1; index < 10; index++) {
+        var imageId = "image" + index;
+        var newImageSrc = "stage" + newStage + "/image" + index + ".jpg";
+        document.getElementById(imageId).src = newImageSrc;
+      }
+    }
+
+    function showAnswerByColumn(num) {
+      var columnId = "result" + num;
+      var answerElement = document.getElementById(columnId);
+      answerElement.style.visibility = "visible";
+    }
   };
-  
-  // function revealAnswer() {
-  //   var answerImages = document.getElementsByClassName('selected');
-  //   var result = document.getElementById('result');
-  
-  //   if (answerImages.length === 0) {
-  //     result.textContent = 'Incorrect!';
-  //   } else {
-  //     var currentStageImages = stages[currentStage];
-  //     var isCorrect = true;
-  
-  //     for (var i = 0; i < answerImages.length; i++) {
-  //       if (!answerImages[i].src.endsWith(currentStageImages[i])) {
-  //         isCorrect = false;
-  //         break;
-  //       }
-  //     }
-  
-  //     if (isCorrect) {
-  //       result.textContent = 'Stage ' + (currentStage + 1) + ': Correct!';
-  //       currentStage++;
-  
-  //       if (currentStage === stages.length) {
-  //         revealButton.disabled = true;
-  //       }
-  //     } else {
-  //       result.textContent = 'Incorrect!';
-  //     }
-  //   }
-  // }
   
