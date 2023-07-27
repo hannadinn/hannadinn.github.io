5b2670b98e128000a67a175a2ee4f3746bd65ff6
Hanadin <junhanlai@gmail.com>
2023-07-27 23:31:33 +0800

    Left with video lol

--- script.js
+++ script.js
@@ -14,38 +14,63 @@ window.onload = function() {
         
         var filePath = this.src;
         if (currentStage === 1 && filePath.includes("3.jpg")) {
-          showAnswerByColumn(2);
-          showAnswerByColumn(3);
           triggerChangeImage(currentStage);
         }
 
         if (currentStage === 2 && filePath.includes("6.jpg")) {
-          showAnswerByColumn(4);
-          showAnswerByColumn(6);
           triggerChangeImage(currentStage)
         }
 
         if (currentStage === 3 && filePath.includes("9.jpg")) {
-          showAnswerByColumn(1);
-          showAnswerByColumn(5);
+          triggerChangeImage(currentStage)
         }
       });
     }
 
     function triggerChangeImage(stage) {
       currentStage = 0;
-      setTimeout(() => changeImage(stage), 1000);
+      document.getElementById("content-container").style.visibility = "hidden";
+      document.getElementById("blur").classList.add("unblurTransition");
+      document.getElementById("blur").style.backdropFilter = "none";
+      setTimeout(() => changeImages(stage), 4000);
       currentStage = stage + 1; 
     }
 
-    function changeImage(stage) {
+    function changeImages(stage) {
       var newStage = stage + 1;
-      var newBgImagePath = 'url(' + "stage" + newStage + "/image" + (newStage*3) + ".jpg)";
-      document.getElementById(bgId).style.backgroundImage = newBgImagePath;
-      for (let index = 1; index < 10; index++) {
-        var imageId = "image" + index;
-        var newImageSrc = "stage" + newStage + "/image" + index + ".jpg";
-        document.getElementById(imageId).src = newImageSrc;
+      if (currentStage <= 3) {
+        for (let index = 1; index < 10; index++) {
+          var imageId = "image" + index;
+          var newImageSrc = "stage" + newStage + "/image" + index + ".jpg";
+          document.getElementById(imageId).src = newImageSrc;
+        }
+        var newBgImagePath = 'url(' + "stage" + newStage + "/image" + (newStage*3) + ".jpg)";
+        document.getElementById(bgId).style.backgroundImage = newBgImagePath;
+      }
+      document.getElementById("blur").classList.remove("unblurTransition");
+      document.getElementById("blur").style.backdropFilter = "blur(8px)";
+      document.getElementById("content-container").style.visibility = "visible";
+      if (currentStage == 2) {
+        showAnswerByColumn(2);
+        showAnswerByColumn(3);
+        document.getElementById("title").innerHTML = "🎄🎄";
+      }
+      if (currentStage == 3) {
+        showAnswerByColumn(4);
+        showAnswerByColumn(6);
+        document.getElementById("title").innerHTML = "😴🤤";
+      }
+      if (currentStage == 4) {
+        showAnswerByColumn(1);
+        showAnswerByColumn(5);
+        var emojiArray = ["😍","🥰", "💓", "🥳", "👰", "👸", "🦊", "🌷", "🍉", "🍙", "🍜", "🥟", "🦀", "🌞", "🎉", "🎊", "💍", "👜", "📸", "♍"];
+        var emojiLine = "";
+        while (emojiArray.length > 0 ) {
+          var rand = emojiArray[(Math.random() * emojiArray.length) | 0];
+          emojiLine += rand;
+          emojiArray = emojiArray.filter(item => item != rand);
+        }
+        document.getElementById("title").innerHTML = emojiLine;
       }
     }
 
